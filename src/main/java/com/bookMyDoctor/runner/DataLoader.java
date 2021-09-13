025e27512820827d5cc6f0866850c220d93de73f
meet63380 <shah.meet6338@gmail.com>
2021-09-13 10:41:54 +0530

    Spring Mini Project

---
+++ src/main/java/com/bookMyDoctor/runner/DataLoader.java
@@ -0,0 +1,25 @@
+//package com.bookMyDoctor.runner;
+//
+//import java.util.Set;
+//
+//import javax.annotation.PostConstruct;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import com.bookMyDoctor.entity.Role;
+//import com.bookMyDoctor.entity.User;
+//import com.bookMyDoctor.repository.UserRepository;
+//
+//@Component
+//public class DataLoader {
+//	 @Autowired
+//	 private UserRepository userRepository;
+//	 
+//	 @PostConstruct
+//	 private void postConstruct() {
+//		 User admin = new User(1, "administrator", "administrator@gmail.com","admin123" , false,
+//					true, true, true, Set<Role> authorities)
+//	 }
+//
+//}
